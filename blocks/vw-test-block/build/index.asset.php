1c603e1aae33f80cec3639b3d634f1446a0e258c
hws-project <ac.hws.worker@gmail.com>
2024-11-26 15:55:45 +0500

    Initial starter

---
+++ blocks/vw-test-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'aa54f4d441dc089d3db8');
---
+++ blocks/vw-test-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6f912703a21fbb67de05');
